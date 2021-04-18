0edf77d3c335ca3756365dd87d4c9d082ba89c21
zurda <m.weizman@gmail.com>
2021-04-18 17:00:06 +0100

    Update allowed_hosts filed
    ALLOWED_HOSTS is a list of strings representing the host/domain names that this Django site can serve. This is a security measure to prevent HTTP Host header attacks, which are possible even under many seemingly-safe web server configurations.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=#ggy7_w+yddpj8d5*7_^2+92iaxtrjo23los&__5!r4(*$&a4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
